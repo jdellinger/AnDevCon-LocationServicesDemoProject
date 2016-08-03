9cd44479602493d7a8acf23e9a321634a1ec521a
jdellinger <jay.dellinger@manheim.com>
2016-08-02 23:53:24 -0400

    added location updates

--- app/src/main/java/com/dellingertech/locationservicesdemo/MainActivity.java
+++ app/src/main/java/com/dellingertech/locationservicesdemo/MainActivity.java
@@ -1,18 +1,39 @@
 package com.dellingertech.locationservicesdemo;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresPermission;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.common.api.GoogleApiClient.*;
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback, OnConnectionFailedListener, ConnectionCallbacks, LocationListener {
 
+    private static final String TAG = "LocationServicesDemo";
+    private static final float START_ZOOM = 12;
+    private static final float UPDATE_ZOOM = 15;
     private GoogleMap mMap;
+    private GoogleApiClient googleApiClient;
+    private Marker marker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +43,13 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
 
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
 
     /**
      * Manipulates the map once available.
@@ -40,7 +66,58 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Boston and move the camera
         LatLng boston = new LatLng(42.346625, -71.084292);
-        mMap.addMarker(new MarkerOptions().position(boston).title("Sheraton Boston"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(boston, 12));
+        updateMap(boston, "Sheraton Boston", START_ZOOM);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.i(TAG, "Connection failed: " + connectionResult.getErrorCode());
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "Connection suspended");
+        googleApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, createLocationRequest(), this);
+    }
+
+    private LocationRequest createLocationRequest() {
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(2000);
+        locationRequest.setFastestInterval(1000);
+        return locationRequest;
+    }
+
+    private void updateMap(LatLng position, String label, float zoom){
+        if(marker == null) {
+            marker = mMap.addMarker(new MarkerOptions()
+                    .position(position)
+                    .title(label)
+            );
+        }
+        marker.setPosition(position);
+        marker.setTitle(label);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position, zoom));
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.i(TAG, String.format("Location changed (%f, %f)", location.getLatitude(), location.getLongitude()));
+        updateMap(new LatLng(location.getLatitude(), location.getLongitude()), "Current Location", UPDATE_ZOOM);
     }
 }
